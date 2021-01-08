aa33343efe8319a59610544e797573917d111108
Nolan Dyke <nolan@Nolans-MacBook-Pro.local>
2021-01-08 01:02:38 -0700

    basic Java notes about loops, arrays, and string methods

--- first_java.java
+++ first_java.java
@@ -110,5 +110,137 @@ used to perform binary logic with bits of an integer or long integer
 | sets each bit to 1 if any of the two bits is 1
 ~ inverts all bits
 ^ sets each bit to 1 if only one of the two bits is 1
+*/
 
+// Java Strings
+
+/*
+String values are surrounded by "double quotes".
+In Java, a String is an object, with methods that can perform operations on a string.
+e.x.:
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the string is: " + txt.length());
+Prints: The length of the txt string is: 26
+
+More string methods:
+.toUpperCase()
+.toLowerCase()
+.indexOf("")
+
+Strings can be concatenated with + and also concat()
+
+Backslash (\) is the escape character
+*/
+
+// Java Math
+
+/*
+Math is a Java class with methods associated with it.
+Math.max()
+.min()
+.sqrt()
+.abs()
+.random()
+*/
+
+// If...else, Switch, While, For, Break/Continue
+
+/* 
+if (condition) {
+
+} else if (condition 2) {
+
+} else {
+
+}
+
+Ternary Operator
+variable = (condition) ? expressionTrue : expressionFalse;
+
+Switch Statement
+
+switch(expression) {
+    case x:
+        // code block
+        break;
+    case y:
+        //code block;
+        break;
+    default:
+        //code block
+}
+
+While loop
+while (condition) {
+
+}
+
+Do While loop
+do {
+
+} while (condition);
+
+For Loop
+
+for (statement 1; statement 2; statement 3){
+    code to be executed
+}
+
+For-Each Loop
+for (type variableName : arrayName){
+    code to be executed
+}
+*/
+
+// Break and Continue
+
+/*
+break can be used to jump out of a loop
+e.x.:
+for (int i = 0; i < 10; i++){
+    if (i == 4) {
+        break;
+    }
+    system.out.println(i);
+}
+
+continue breaks one iteration in the loop if a condition is true and continues 
+with the next iteration in the loop
+e.x.:
+for(int i = 0; i < 10; i++){
+    if (i==4){
+        continue;
+    }
+    system.out.println(i);
+}
+prints: 0 1 2 3 5 6 7 8 9
+*/
+
+// Java Arrays
+
+/*
+declared by defining the variable type with square brackets
+
+String[] cars;
+
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+int[] myNum = {10, 20, 30, 40};
+
+values of an array can be changed my referencing the element
+cars[0] = "Opel";
+
+can find array length using .length
+
+Loop through an Array
+for (int i = 0; i < cars.length; i++){
+    code block
+}
+
+for (type variable : arrayname) {
+    ...
+}
+
+Multidimensional Arrays
+
+int[][] myNumbers = {{1, 2 3, 4}, {5, 6, 7}};
 */
